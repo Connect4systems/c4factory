cfd4c8b79a5037c29fbef01e6cfcce1b8594c2f1
Connect4 <connect4systems@gmail.com>
2026-03-16 14:16:25 +0200

    create bom

--- c4factory/c4factory/doctype/contract_bom_request/contract_bom_request.js
+++ c4factory/c4factory/doctype/contract_bom_request/contract_bom_request.js
@@ -1,8 +1,47 @@
 // Copyright (c) 2025, Connect 4 Systems and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Contract BOM Request", {
-// 	refresh(frm) {
+frappe.ui.form.on("Contract BOM Item", {
+	create_bom: function(frm, cdt, cdn) {
+		let row = locals[cdt][cdn];
 
-// 	},
-// });
+		if (!row.item) {
+			frappe.msgprint(__("Please set an Item in the row before creating a BOM."));
+			return;
+		}
+
+		if (row.bom) {
+			frappe.confirm(
+				__("A BOM already exists for this row ({0}). Create a new one anyway?", [row.bom]),
+				function() { do_create(frm, cdt, cdn, row); }
+			);
+			return;
+		}
+
+		do_create(frm, cdt, cdn, row);
+	}
+});
+
+function do_create(frm, cdt, cdn, row) {
+	frappe.call({
+		method: "c4factory.c4factory.doctype.contract_bom_request.contract_bom_request.create_bom_for_item",
+		args: {
+			item: row.item,
+			qty: row.qty || 1,
+			company: frm.doc.company || frappe.defaults.get_default("company")
+		},
+		freeze: true,
+		freeze_message: __("Creating BOM..."),
+		callback: function(r) {
+			if (r.message) {
+				frappe.model.set_value(cdt, cdn, "bom", r.message);
+				frm.save().then(function() {
+					frappe.show_alert({
+						message: __("BOM {0} created and saved.", [r.message]),
+						indicator: "green"
+					});
+				});
+			}
+		}
+	});
+}
--- c4factory/c4factory/doctype/contract_bom_request/contract_bom_request.py
+++ c4factory/c4factory/doctype/contract_bom_request/contract_bom_request.py
@@ -1,9 +1,25 @@
 # Copyright (c) 2025, Connect 4 Systems and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class ContractBOMRequest(Document):
 	pass
+
+
+@frappe.whitelist()
+def create_bom_for_item(item, qty=1, company=None):
+	if not company:
+		company = frappe.defaults.get_default("company")
+
+	bom = frappe.get_doc({
+		"doctype": "BOM",
+		"item": item,
+		"quantity": frappe.utils.flt(qty) or 1,
+		"company": company,
+	})
+	bom.insert(ignore_permissions=False)
+	frappe.db.commit()
+	return bom.name
