b11f7b646280aaa08d66e43606436c47cd8be716
Connect4 <connect4systems@gmail.com>
2026-05-18 19:36:46 +0300

    pp

--- c4factory/api/pick_list_dashboard.py
+++ c4factory/api/pick_list_dashboard.py
@@ -3,6 +3,10 @@ from frappe import _
 
 def get_data(*args, **kwargs):
     return {
+        "fieldname": "pick_list",
+        "non_standard_fieldnames": {
+            "Job Card": "custom_pick_list",
+        },
         "transactions": [
             {
                 "label": _("Manufacturing"),
